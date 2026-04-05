a1bca422738102879d059c5c04fe2fe32cd12bfe
Mohammad Balsaniya <122770062+mohammedbalsaniya@users.noreply.github.com>
2026-04-05 12:22:59 +0530

    change to proper logic

--- BinarySearch.java
+++ BinarySearch.java
@@ -2,24 +2,24 @@ class BinarySearch{
   public static void main(String[] args) {
     int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int target = 5;
+    int low = 0;
     int lenght = arr.length;
     int mid = lenght / 2;
-    if(target == arr[mid]) {
-        System.out.println("Element found at index: " + mid);
-    } else if (target < arr[mid]) {
-        for (int i = 0; i < mid; i++) {
-            if (arr[i] == target) {
-                System.out.println("Element found at index: " + i);
-                return;
-            }
-        }
-    } else {
-        for (int i = mid + 1; i < lenght; i++) {
-            if (arr[i] == target) {
-                System.out.println("Element found at index: " + i);
-                return;
-            }
-        }
-    }
+
+  while(mid <= length){
+
+      if(target == arr[mid]){
+        print("element found at " + arr[mid]);
+      }
+      if(target <= mid){
+          lenght = mid-1;
+      } else if(target >= mid){
+          low = mid +1;
+      }
+  }
+        
+      function print(p){
+         System.out.println(n);
+      }
   }
 }
